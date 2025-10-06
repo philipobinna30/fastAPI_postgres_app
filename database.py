2565636a64a44cbb257d141f5448c5b3e6005d3f
philip obinna <anuforophilipobinna@yahoo.com>
2025-10-06 14:26:11 -0700

    first commit

--- database.py
+++ database.py
@@ -1,14 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://myuser:mypassword@localhost:5432/mydb"
 
-
-# Create engine
-engine = create_engine(DATABASE_URL)
 
 # Create session
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False)
 
 # Base class for models
 Base = declarative_base()
@@ -21,11 +17,9 @@ import os
 load_dotenv()
 
 # Access the values
-db_url = os.getenv("DATABASE_URL")
 secret_key = os.getenv("SECRET_KEY")
 api_key = os.getenv("API_KEY")
 
-print("Database URL:", db_url)
 print("Secret Key:", secret_key)
 print("API Key:", api_key)
 
